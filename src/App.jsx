62442f9ffcaf94a749f44182ae456e928dbf7430
Phool Kumari <phoolkumari7333@gmail.com>
2026-07-14 16:08:01 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,19 @@
 import React, { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [name, setName] = useState("pooja");
 
   return (
     <div className="flex  flex-col items-center gap-4 p-5">
       <div className="bg-green-600 h-20 w-20 rounded-2xl flex items-center justify-center text-white text-2xl font-bold">
-        {count}
+        {name}
       </div>
 
       <button
-        onClick={() => setCount(count + 1)}
+        onClick={() => setName("neha")}
         className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600"
       >
-        Increment
+        Change Name
       </button>
     </div>
   );
